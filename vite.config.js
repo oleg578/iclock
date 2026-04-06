7bd083a31cc3e5b957e3ee8df2004196d4e3b84b
oleg578 <oleg.nagornij@gmail.com>
2026-04-06 11:12:51 +0200

    add server configuration to Vite for host settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   base: './',
+  server: {
+    host: '0.0.0.0',
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
